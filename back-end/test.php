cc96f5e94e16e7acf30c5fee792156771db31564
Luís Filipe Medeiros Tomé <fc48341@alunos.fc.ul.pt>
2022-03-26 20:13:20 +0000

    php file changed

--- back-end/test.php
+++ back-end/test.php
@@ -1,19 +1,16 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:sqldb05server1.database.windows.net,1433; Database = sqldb1", "ptrptisqldb", "{2SdULWb5ePk83jA}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ptrptisqldb", "pwd" => "{2SdULWb5ePk83jA}", "Database" => "sqldb1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:sqldb05server1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-$tsql= "SELECT TOP (10) * FROM [SalesLT].[Product]";
+    $serverName = "your_server.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "your_database", // update me
+        "Uid" => "your_username", // update me
+        "PWD" => "your_password" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
@@ -22,4 +19,6 @@ $tsql= "SELECT TOP (10) * FROM [SalesLT].[Product]";
      echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
+
+    echo ("test of string");
 ?>
