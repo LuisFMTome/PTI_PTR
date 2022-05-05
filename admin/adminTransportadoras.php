14afcd15723a1b1e24a6d4637c393c25cbc42124
jonnyviana <79712833+jonnyviana@users.noreply.github.com>
2022-05-05 16:09:29 +0100

    Erro corrigido

--- admin/adminTransportadoras.php
+++ admin/adminTransportadoras.php
@@ -189,7 +189,7 @@
                               <td><?php echo $row['morada'] ?></td>
                               <td><?php echo $row['codigoPostal'] ?></td>
                           </tr>
-                        }
+                          <?php } ?>
 
                     </tbody>
                     <tfoot>
