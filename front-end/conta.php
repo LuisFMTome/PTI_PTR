4eff5c50964e3b8c3c1cb40178e63b127320e4ce
Goncalo <goncalomocf@gmail.com>
2022-05-09 15:57:58 +0100

    avanco no registo de transportes

--- front-end/conta.php
+++ front-end/conta.php
@@ -17,6 +17,7 @@ session_start();
     <!--<link rel="icon" type="image/x-icon" href="assets/favicon.ico" /> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <link href="style.css" rel="stylesheet" />
+    <script src="js/libs/jquery.min.js" type="text/javascript"></script>
     <!--<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="sweetalert2.all.min.js"></script> -->
 
@@ -67,7 +68,7 @@ session_start();
                     <h2>Registar</h2>
                     <form id="RegisterForm" action="registar.php" method="post">
                         <label for="users">Escolha o tipo de utilizador:</label>
-                        <select id="choose" name="tipoUtili" onchange="getOption()">
+                        <select id="choose" name="tipoUtili">
                             <option value="comprador">Consumidor</option>
                             <option value="transportadora">Transportadora</option>
                             <option value="fornecedor">Fornecedor</option>
