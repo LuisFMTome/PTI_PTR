a74b7b67beec17472ad4be821c6b0636b4af4e29
LuisFMTome <100805884+LuisFMTome@users.noreply.github.com>
2022-06-27 15:41:20 +0100

    Transacoes por PayPal - fase teste

--- front-end/carrinho.php
+++ front-end/carrinho.php
@@ -149,8 +149,12 @@ $row_count = sqlsrv_num_rows($query);
 
                         $idT = $values["item_id"];
 
-                        $query = "SELECT * FROM [dbo].[Produto] WHERE pid='{$idT}'";
+                        $query = "SELECT p.nome, f.paypalid  
+                        FROM [dbo].[Produto] p, [dbo].[Fornecedor] f, [dbo].[Armazem] a
+                        WHERE p.pid='{$idT}' AND p.morada=a.morada AND a.fornecedor=f.fid";
                         $result = sqlsrv_query($conn, $query);
+
+                        echo $result;
 
                         if( $result === false ) {
                             die( print_r( sqlsrv_errors(), true));
@@ -159,7 +163,7 @@ $row_count = sqlsrv_num_rows($query);
                             die( print_r( sqlsrv_errors(), true));
                         }
 
-                        $nomeP = sqlsrv_get_field( $result, 1);
+                        $nomeP = sqlsrv_get_field( $result, 0);
 
                         ?>
                         <tr>
