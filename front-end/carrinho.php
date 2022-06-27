6b8d5b05f0e22abb9df6f6ac86eca4bec45d4262
LuisFMTome <100805884+LuisFMTome@users.noreply.github.com>
2022-06-27 16:11:45 +0100

    Transacoes por PayPal - fase teste

--- front-end/carrinho.php
+++ front-end/carrinho.php
@@ -166,7 +166,7 @@ $row_count = sqlsrv_num_rows($query);
 
                         ?>
                         <tr>
-                            <td><?php echo $nomeP; ?></td>
+                            <td><?php echo $nomeP?></td>
                             <!--<td><input type="number" value="1"></td>-->
                             <td><?php echo $values["item_price"] . "€" ?></td>
                             <td><a href="carrinho.php?action=delete&id=<?php echo $values["item_id"]; ?>">Remover</a></td>
@@ -252,7 +252,8 @@ $row_count = sqlsrv_num_rows($query);
     </div>
     </main>
 <script src=<?php echo "https://www.paypal.com/sdk/js?client-id=".$paypalid."&disable-funding=credit,card&currency=EUR"?>></script>    <script>
-        var price = <?php echo $total ?>;
+        var price = document.getElementById("total");
+        price = price.slice(0, -1);
         console.log(price.innerText);
         paypal.Buttons({
             style : {
